f132bc14dac78e9bcdf8b0931020f8aeddb58cb0
Paul O'Connor <paul@poconnor.me>
2014-08-05 17:25:57 +0100

    Relay list

--- manifests/init.pp
+++ manifests/init.pp
@@ -301,6 +301,7 @@ class graphite (
   $additional_servers        = '',
   $web_cors_allow_from_all   = true,
   $use_remote_user_auth      = $graphite::params::use_remote_user_auth,
+  $relay_count               = [],
 ) inherits graphite::params {
   # Validation of input variables.
   # TODO - validate all the things
