3d9158822403e6ff711d797a1eb6c2c84eddb771
Paul O'Connor <paul@poconnor.me>
2014-08-21 11:20:06 +0100

    Top level

--- manifests/install/package.pp
+++ manifests/install/package.pp
@@ -11,22 +11,22 @@ class graphite::install::package (
       )inherits graphite::params {
 
   package{'graphite-web':
-    ensure   => $::graphite::graphiteVersion,
+    ensure   => $graphite::graphiteVersion,
   }->
   package{'carbon':
-    ensure   => $::graphite::carbonVersion,
+    ensure   => $graphite::carbonVersion,
   }->
   package{'whisper':
-    ensure   => $::graphite::whisperVersion,
+    ensure   => $graphite::whisperVersion,
   }->
 
   # workaround for unusual graphite install target:
   # https://github.com/graphite-project/carbon/issues/86
-  file { $::graphite::params::carbin_pip_hack_source :
+  file { $graphite::params::carbin_pip_hack_source :
     ensure => link,
     target => $::graphite::params::carbin_pip_hack_target,
   }->
-  file { $::graphite::params::gweb_pip_hack_source :
+  file { $graphite::params::gweb_pip_hack_source :
     ensure => link,
     target => $::graphite::params::gweb_pip_hack_target,
   }
